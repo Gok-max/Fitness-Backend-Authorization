bbeb2d41daafd90bbd383f39f47ffc6e8a075f0b
Gok-max <145645126+Gok-max@users.noreply.github.com>
2024-12-23 18:22:16 +0530

    Add files via upload

---
+++ src/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import config from "./utils/config.js";
+import cors from "cors";
+import AppRoutes from "./routes/index.js";
+
+const app = express();
+
+// const corsOptions = {
+//   origin:
+//     "https://online-personalized-fitness-class-booking-platform-fit-pro.vercel.app", // Remove trailing slash
+//   methods: ["GET", "POST", "PUT", "DELETE"],
+//   allowedHeaders: ["Content-Type", "Authorization"],
+//   //   credentials: true, // Allow cookies to be sent with cross-origin requests
+// };
+
+app.use(cors());
+app.use(express.json({ limit: "5mb" }));
+
+app.use(AppRoutes);
+
+app.listen(config.PORT, () =>
+  console.log(`Server listening on port ${config.PORT}`)
+);
