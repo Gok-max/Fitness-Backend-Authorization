276522304a123f42c6afc9abf6d0a3e1f22793c2
Gok-max <145645126+Gok-max@users.noreply.github.com>
2024-12-24 20:47:23 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,7 +13,10 @@ const app = express();
 //   //   credentials: true, // Allow cookies to be sent with cross-origin requests
 // };
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173', // Replace with your frontend's URL
+  credentials: true, // If using cookies or authorization headers
+}));
 app.use(express.json({ limit: "5mb" }));
 
 app.use(AppRoutes);
