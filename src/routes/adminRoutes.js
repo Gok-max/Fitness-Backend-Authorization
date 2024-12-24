2cf3968db4b5199e5dd007e78bc1e99a2189ca09
Gok-max <145645126+Gok-max@users.noreply.github.com>
2024-12-24 20:52:18 +0530

    Update adminRoutes.js

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import adminController from "../controller/adminController.js";
-import verifyAuth from "../middleware/verifyAuth";
+import verifyAuth from "../middleware/verifyAuth.js";
 import verifyAdmin from "../middleware/verifyAdmin.js";
 const router = express.Router();
 
