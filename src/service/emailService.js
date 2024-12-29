e7326fe08ae111418a038403c0ff42cbde13c95c
Gok-max <145645126+Gok-max@users.noreply.github.com>
2024-12-29 15:18:32 +0530

    Update emailService.js

--- src/service/emailService.js
+++ src/service/emailService.js
@@ -3,7 +3,7 @@ import transporter from "../utils/nodemailerConfig.js";
 export const sendEmail = async (to, subject, text) => {
   try {
     await transporter.sendMail({
-      from: process.env.EMAIL_USER,
+      from: 'gokulven01@gmail.com',
       to,
       subject,
       text,
@@ -11,5 +11,6 @@ export const sendEmail = async (to, subject, text) => {
     console.log(`Email sent to ${to}`);
   } catch (error) {
     console.error(`Error sending email: ${error.message}`);
+    console.log(to,subject,text,'to');
   }
 };
