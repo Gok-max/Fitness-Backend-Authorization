64ffb2130e0a9bab395e46b2a6a06e04211ad622
Gok-max <145645126+Gok-max@users.noreply.github.com>
2024-12-29 15:19:19 +0530

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -9,4 +9,6 @@ export default {
   JWT_EXPIRY: process.env.JWT_EXPIRY,
   EMAIL_USER: process.env.EMAIL_USER,
   EMAIL_PASS: process.env.EMAIL_PASS,
+  RAZORPAY_KEY_ID:process.env.RAZORPAY_KEY_ID,
+  RAZORPAY_KEY_SECRET:process.env.RAZORPAY_KEY_SECRET
 };
