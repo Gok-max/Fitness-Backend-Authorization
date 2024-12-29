9f90cf2a490674f0782008922412a4f4f9c9cf9e
Gok-max <145645126+Gok-max@users.noreply.github.com>
2024-12-29 15:13:42 +0530

    Update index.js

--- src/model/index.js
+++ src/model/index.js
@@ -4,7 +4,18 @@ import config from "../utils/config.js";
 main().catch((error) => console.error("MongoDB Connection Failed", error));
 
 async function main() {
+  console.log(`${config.DB_URL}/${config.DB_NAME}`);import mongoose from "mongoose";
+import config from "../utils/config.js";
+
+main().catch((error) => console.error("MongoDB Connection Failed", error));
+
+async function main() {
   console.log(`${config.DB_URL}/${config.DB_NAME}`);
+  await mongoose.connect(`${config.DB_URL}/${config.DB_NAME}`);
+  console.log("MongoDB Connected!");
+}
+
+export default mongoose;
   await mongoose.connect(`${config.DB_URL}/${config.DB_NAME}`);
   console.log("MongoDB Connected!");
 }
